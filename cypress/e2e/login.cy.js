7d2d1fc242a67db966324693aba14a500bf221f3
unknown <seuemail@gmail.com>
2026-07-17 18:27:44 -0300

    Fix selectors typo and worng assertion in login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,31 @@
 describe('Login OrangeHRM', () => {
+
+  const selectorsList ={
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    wrongCredentialAlert: "[role='alert']"
+  }
+
   it('Deve realizar login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
-
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.url().should('include', '/dashboard')
-    cy.contains('Dashboard').should('be.visible')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
 
  it('Deve exibir mensagem de erro com credenciais inválidas', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-    cy.get('input[name="username"]').type('usuarioInvalido')
-    cy.get('input[name="password"]').type('senhaErrada123')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type('usuarioInvalido')
+    cy.get(selectorsList.passwordField).type('senhaErrada123')
+    cy.get(selectorsList.loginButton).click()
 
-    cy.contains('Invalid credentials').should('be.visible')
-    cy.url().should('include', '/auth/login')
+    cy.get(selectorsList.wrongCredentialAlert).should('contain', 'Invalid credentials')
+  cy.url().should('include', '/auth/login')
   })
 })
